5537314f9aea7a87359cc073f4cf8976ec22c5d8
Ahmed sabri <AhmedSabry@gmail.com>
2025-03-08 10:35:20 +0200

    add properties in class category

--- ClassLibrary/Category.cs
+++ ClassLibrary/Category.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary
 {
-    class category
-    {
+   public  class Category
+    { 
+        //add properties in class category
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+        public string CategoryDescription { get; set; }
     }
 }
